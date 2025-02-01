ee82677f62f8e78f5562e0c52f71a10d2d9dff68
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-02-01 16:49:13 +0330

    add button

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -11,30 +11,11 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             
-            LinearGradient(colors: [.blue, Color("lightBlue")],
-                           startPoint: .top,
-                           endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            
+            BackgroundView(colorTop: .blue, colorBottom: Color("lightBlue"))
             VStack{
-                Text("Cupertino, CA")
-                    .font(.system(size: 32, weight: .bold, design: .default))
-                    .foregroundColor(.white)
-                    .padding()
-                VStack(spacing: 8){
-                    Image(systemName: "cloud.sun.fill")
-                        .renderingMode(.original)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 180, height: 180)
-                    
-                    Text("76°")
-                        .font(.system(size: 70, weight: .medium))
-                        .foregroundColor(.white)
-                    
-                }
-                .padding(.bottom, 50)
-              
+                CityTextView(cityName: "Cupertino, CA")
+                
+                MainWeatherStatusView(imageName: "cloud.sun.fill", temperature: 76)
                 
                 HStack(spacing: 15){
                     WeatherDayView(dayOfWeek: "TUE",
@@ -57,6 +38,14 @@ struct ContentView: View {
                 
                 Spacer()
                 
+                Button{
+                    print("Tapped")
+                }label: {
+                    WeatherButton(title:"Change Day Time",
+                                  backGroundColor: .white,
+                                  textColor: .blue)
+                }
+                Spacer()
             }
         }
      
@@ -89,5 +78,66 @@ struct WeatherDayView: View {
                 .font(.system(size: 28, weight: .medium))
                 .foregroundColor(.white)
         }
+    }
+}
+
+struct BackgroundView: View {
+    
+    var colorTop: Color
+    var colorBottom: Color
+    
+    var body: some View {
+        LinearGradient(colors: [colorTop,colorBottom],
+                       startPoint: .top,
+                       endPoint: .bottom)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    var body: some View{
+        Text(cityName)
+            .font(.system(size: 32, weight: .bold, design: .default))
+            .foregroundColor(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack(spacing: 8){
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 180, height: 180)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundColor(.white)
+            
+        }
+        .padding(.bottom, 50)
+      
+    }
+}
+
+struct WeatherButton: View {
+    var title: String
+    var backGroundColor: Color
+    var textColor: Color
+    
+    var body: some View{
+        Text(title)
+            .frame(width: 280,height: 50)
+            .background(backGroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20,weight: .bold, design: .default))
+            .cornerRadius(10)
     }
 }
